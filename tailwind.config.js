8e7b069193c136df8a362b11faab8da4f98be7b0
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-10-18 08:47:58 +0200

    Template

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/"], // updated line here!
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
