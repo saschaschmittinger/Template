cce368dcd7a010d81de6e2d82c84a98d465d1542
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-10-18 20:54:15 +0200

    18_10_24

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import FrontPageView
+from .views import FrontPageView, ShopPageView
 
 app_name = 'core'
 
 urlpatterns = [
     path('', FrontPageView.as_view(), name='index_view'),
+    path('shop/', ShopPageView.as_view(), name='shop_view'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,9 +3,10 @@ from django.views import generic
 from product.models import Product
 
 
+
 class FrontPageView(generic.ListView):
     title = 'SSC E-Commerce'
-    queryset = Product.objects.all()
+    queryset = Product.objects.all()[0:8]
     template_name = 'core/index.html'
     context_object_name = 'products'
     
@@ -13,3 +14,28 @@ class FrontPageView(generic.ListView):
         context = super(FrontPageView, self).get_context_data(**kwargs)  
         context.update({'title': self.title})  
         return context
+
+
+
+class ShopPageView(generic.ListView):
+    title = 'SSC Shop'
+    queryset = Product.objects.all()
+    template_name = 'shop/shop_list.html'
+    context_object_name = 'products'
+    
+    def get_context_data(self, **kwargs):
+        context = super(ShopPageView, self).get_context_data(**kwargs)  
+        context.update({'title': self.title})  
+        return context
+    
+
+
+class ShopDetailView(generic.DetailView):
+    title = 'SSC Detail View'
+    queryset = Product.objects.all()
+    context_object_name ='product'
+    
+    def get_context_data(self, **kwargs):
+       context = super(ShopDetailView, self).get_context_data(**kwargs)  
+       context.update({'title': self.title})  
+       return context
