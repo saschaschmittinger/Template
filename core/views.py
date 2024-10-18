440eaaa48a21334a729cfbb99d701cf6bb4d51e8
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-10-18 16:13:22 +0200

    18_10_24

--- core/views.py
+++ core/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
+from product.models import Product
 
 
-class FrontPageView(generic.TemplateView):
+class FrontPageView(generic.ListView):
     title = 'SSC E-Commerce'
+    queryset = Product.objects.all()
     template_name = 'core/index.html'
+    context_object_name = 'products'
     
     def get_context_data(self, **kwargs):
         context = super(FrontPageView, self).get_context_data(**kwargs)  
